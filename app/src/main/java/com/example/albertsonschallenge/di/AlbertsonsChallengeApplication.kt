9290aed4fd217bccb505d9854995e3d905eb887b
oceasia1622 <bdk1389@comcast.net>
2022-10-06 09:02:00 -0500

    10/6
    Added unit test example

--- app/src/main/java/com/example/albertsonschallenge/di/AlbertsonsChallengeApplication.kt
+++ app/src/main/java/com/example/albertsonschallenge/di/AlbertsonsChallengeApplication.kt
@@ -1,14 +1,7 @@
 package com.example.albertsonschallenge.di
 
 import android.app.Application
-import com.example.albertsonschallenge.model.Repository
-import com.example.albertsonschallenge.model.RepositoryImplementation
-import com.example.albertsonschallenge.model.remote.SearchAPI
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.components.ViewModelComponent
 
 @HiltAndroidApp
 class AlbertsonsChallengeApplication: Application()
